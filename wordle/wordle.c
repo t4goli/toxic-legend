3f08a06b785d3fea531ad62e7ca29dbd44ea63c2
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-15 18:01:53 +0000

    Sun, Jan 15, 2023, 6:01 PM +00:00

--- wordle/wordle.c
+++ wordle/wordle.c
@@ -90,9 +90,9 @@ int main(int argc, string argv[])
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
 
-        for (int i = 0; i < wordsize; i++)
+        for (int j = 0; j < wordsize; j++)
         {
-            status[i] = 0;
+            status[j] = 0;
         }
 
         // Calculate score for the guess
