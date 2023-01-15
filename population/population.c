1a1d117512a70a2c735bfc4ba1482b7d7fb9f8e8
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-15 00:10:20 +0000

    Sun, Jan 15, 2023, 12:10 AM +00:00

--- population/population.c
+++ population/population.c
@@ -4,9 +4,21 @@
 int main(void)
 {
     // TODO: Prompt for start size
+    int start;
+    do
+    {
+        start = get_int("Start size: ");
+    }
+    while (n > 9);
 
     // TODO: Prompt for end size
-
+    int end;
+    do
+    {
+        end = get_int("End size: ");
+    }
+    while (end < start);
+    
     // TODO: Calculate number of years until we reach threshold
 
     // TODO: Print number of years
