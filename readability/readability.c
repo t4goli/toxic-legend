0a5f0978beaa9de97d1ad4ff3888302840484658
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-15 07:29:44 +0000

    Sun, Jan 15, 2023, 7:29 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,22 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(string text)
+int count_words(string text)
+int count_sentences(string text)
+
+
+
 int main(void)
 {
     string text = get_string("Text: ");
 
+}
+
+int count_letters(string text)
+{
+    for (int i = 0; i < strlen(text); i++)
+    {
+        
+    }
 }
