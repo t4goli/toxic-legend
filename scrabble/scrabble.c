4ff4d6fe11b5821e5a3e7e6f402d7fa2c7e81007
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-15 06:50:10 +0000

    Sun, Jan 15, 2023, 6:50 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -24,4 +24,10 @@ int main(void)
 int compute_score(string word)
 {
     String CAP[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z}
+    String LOW[] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}
+
+    for (int i = 0; i < 26; i++)
+    {
+        if 
+    }
 }
