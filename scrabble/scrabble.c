80ff8cfdd0422cb01b54a7ce3a6d9258a6265805
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-15 07:00:22 +0000

    Sun, Jan 15, 2023, 7:00 AM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -35,7 +35,7 @@ int main(void)
 
 int compute_score(string word)
 {
-    char CAP[] = {A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z}
+    char CAP[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
     char LOW[] = {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}
 
     int count = 0;
