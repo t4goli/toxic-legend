51c91508d34e5b426e8c36f64b870cde1cf4fb18
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-16 05:39:37 +0000

    Mon, Jan 16, 2023, 5:39 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -127,7 +127,16 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (preferences[i][j] > preferences[j][i])
+            {
+                
+            }
+        }
+    }
     return;
 }
 
