bd742abe03b134c010be9a20ade5e509f5849ff6
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-16 05:14:27 +0000

    Mon, Jan 16, 2023, 5:14 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -81,7 +81,7 @@ bool vote(string name)
 // Print the winner (or winners) of the election
 void print_winner(void)
 {
-    int high = 0;
+    int high = candidates[0].votes;
     for (int i = 0; i < candidate_count; i++)
     {
         int k = candidates[i].votes;
