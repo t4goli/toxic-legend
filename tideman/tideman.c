a66fe99217fa5129c6a33494451d605dc96beec8
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-16 05:56:42 +0000

    Mon, Jan 16, 2023, 5:56 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -145,19 +145,6 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    int tempw;
-    int templ;
-    for (int i = 0; i < pair_count; i++)
-    {
-        for (int j = 0; j < pair_count; j++)
-        {
-            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser])
-            {
-                tempw = pairs[i].winner;
-                templ = pairs[i].loser;
-            }
-        }
-    }
     return;
 }
 
