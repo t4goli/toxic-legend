fe5ccb85796335801e1d2e0e8589408163ce141d
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-21 20:59:30 +0000

    Sat, Jan 21, 2023, 8:59 PM +00:00

--- smiley/helpers.h
+++ smiley/helpers.h
@@ -3,13 +3,12 @@
 // colorize image
 void colorize(int height, int width, RGBTRIPLE image[height][width]);
 {
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            RGBTRIPLE image[i][j].rgbtBlue = 3;
-            RGBTRIPLE image[i][j].rgbtBlue = 252;
-            RGBTRIPLE image[i][j].rgbtBlue = 132;
+            RGBTRIPLE p =
         }
     }
 }
