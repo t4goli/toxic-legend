6330f68f477a5dd2dfbf99efa633184635b269c4
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-21 21:04:28 +0000

    Sat, Jan 21, 2023, 9:04 PM +00:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -2,5 +2,14 @@
 
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
-    // Change all black pixels to a color of your choosing
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE p = image[i][j];
+            p.rgbtRed = 3;
+            p.rgbtGreen = 252;
+            p.rgbtBlue = 132;
+        }
+    }
 }
--- smiley/helpers.h
+++ smiley/helpers.h
@@ -2,17 +2,4 @@
 
 // colorize image
 void colorize(int height, int width, RGBTRIPLE image[height][width]);
-{
-
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            RGBTRIPLE p = image[i][j];
-            p.rgbtRed = 3;
-            p.rbgtGreen = 252;
-            p.rgbtBlue = 132;
-        }
-    }
-}
 
