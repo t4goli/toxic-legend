acf853dfffa5012022a2a42e3a61a94c91a8cc5a
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-22 04:23:02 +0000

    Sun, Jan 22, 2023, 4:23 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,7 +35,8 @@ int main(int argc, char *argv[])
 
     // TODO: Copy header from input file to output file
     uint8_t header[44];
-    fread()
+    fread(void *header, size_t 2, size_t 44, FILE *input.wav)
+    fwrite(void *header)
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
