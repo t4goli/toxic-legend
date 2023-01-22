4b1afb08c072e8362e92d95528f27aa85f8fb197
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-22 19:53:32 +0000

    Sun, Jan 22, 2023, 7:53 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -19,6 +19,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for ()
     return;
 }
 
