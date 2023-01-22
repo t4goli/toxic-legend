f0ded6be3921701e4274ae842b4be18a46b0ae89
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-22 21:05:31 +0000

    Sun, Jan 22, 2023, 9:05 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -39,12 +39,19 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
 
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+
     return;
 }
