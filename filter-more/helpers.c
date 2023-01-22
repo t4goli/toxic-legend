fb69cde63f8089e55e7149d6ce522d87a396282f
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-22 22:01:30 +0000

    Sun, Jan 22, 2023, 10:01 PM +00:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -22,7 +22,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < (round)(width / 2); j++)
+        for (int j = 0; j < (width / 2); j++)
         {
             int b = image[i][j].rgbtBlue;
             int r = image[i][j].rgbtRed;
