4bf3824d4691305cb618eb85b3ed0d3795e0da9f
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-22 04:38:10 +0000

    Sun, Jan 22, 2023, 4:38 AM +00:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,7 +39,16 @@ int main(int argc, char *argv[])
     fwrite(&header[0], 2, 44, output);
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-
+    int s = 1;
+    do
+    {
+        s = fread(&buffer, 2, 1, input);
+        if (s != 0)
+        {
+            fwrite(&buffer, 2, 1, output);
+        }
+    }
+    while (s != 0);
     // Close files
     fclose(input);
     fclose(output);
