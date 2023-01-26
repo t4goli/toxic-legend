02caec91c416510e191049db3d6bd50517dced6f
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-26 07:14:44 +0000

    Thu, Jan 26, 2023, 7:14 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -6,8 +6,14 @@ int main(int argc, char *argv[])
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
+        return 2;
     }
     char *infile = argv[1];
 
-    FILE *inptr = fopen()
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 4;
+    }
 }
