4ceb8e6eaaf33ce8e2bd0c45e4670304066d63f9
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-26 09:24:41 +0000

    Thu, Jan 26, 2023, 9:24 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
     int l = 0;
     typedef uint8_t  BYTE;
     BYTE buffer[512];
-    char *fn = malloc(4);
+    char *fn = malloc(32);
     while(fread(buffer, sizeof(uint8_t), 512, inptr) == 512)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
