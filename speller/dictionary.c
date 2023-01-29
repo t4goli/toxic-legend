14e05cd89d97646f481df8293c2f7cb15f863ebb
t4goli <122705399+t4goli@users.noreply.github.com>
2023-01-29 22:42:33 +0000

    Sun, Jan 29, 2023, 5:42 PM -05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -36,7 +36,7 @@ unsigned int hash(const char *word)
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    FILE *intpr = fopen(infile, "r")
+    FILE *intpr = fopen(dictionary, "r")
     if (intpr = NULL)
     {
         return false;
