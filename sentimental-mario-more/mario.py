a0a871b1e408a2d267af5ef89abc9b5cd3e94bf1
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-03 05:15:01 +0000

    Fri, Feb 3, 2023, 12:15 AM -05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,15 +1,22 @@
 # TODO
 from cs50 import get_int
 
-num = get_height()
+def main():
+    num = get_height()
+    for i in range(1, num + 1):
+        for j in range(num - i, 0, -1):
+            print(" ", end="")
+        for l in range(i):
+            print("#", end="")
+        print("  ", end="")
+        for k in range(i):
+            print("#", end="")
+        print()
 
-for i in range(1, num + 1)
-    a =
-    for j in range(num - i, 0, -1)
-        print
-
-def get_height()
+def get_height():
     while True:
         num = get_int("Height: ")
-        if num > 0 and num < 9
+        if num > 0 and num < 9:
             return num
+
+main()
