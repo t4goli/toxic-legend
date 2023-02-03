38603b4bf1db02364fe7094cfb372b0433cb4cc1
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-03 05:08:49 +0000

    Fri, Feb 3, 2023, 12:08 AM -05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -3,8 +3,8 @@ from cs50 import get_int
 
 num = get_height()
 
-for i in range(num)
-    for j = num - 1 and j in range(num)
+for i in range(1, num + 1)
+    for j in range(num - i, 0, )
         print
 
 def get_height()
