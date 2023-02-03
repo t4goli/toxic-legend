5b6d3ac525df0e68658edea034ce5244071f0339
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-03 04:07:28 +0000

    Thu, Feb 2, 2023, 11:07 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,7 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
-    
+    with open(argv[2], "a") as file:
+        
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
