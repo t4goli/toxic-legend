2ca24973819167d3cde6105c98137fca075a4517
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-03 05:24:11 +0000

    Fri, Feb 3, 2023, 12:24 AM -05:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -2,6 +2,17 @@
 from cs50 import get_int
 
 def main():
+    length = 1
+    pholder = get_num()
+    for i in range(1, pholder, *10)
+        length += 1
+        pholder / 10
+    if length % 2 is 1
+        plength = length + 1
+    else
+        plength = length
+    for i in range
+
 
 def get_num():
     while True:
