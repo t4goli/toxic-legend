9d5c1e7762d5b92257461c954cae7f6f3893ea69
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-03 04:26:12 +0000

    Thu, Feb 2, 2023, 11:26 PM -05:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -26,8 +26,12 @@ def main():
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
     for i in range(N)
-        simulate_tournament(teams)
-        
+        w = simulate_tournament(teams)
+        if w in counts:
+            counts[w] += 1
+        else:
+            counts[w] = 1
+
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
@@ -58,7 +62,7 @@ def simulate_round(teams):
 
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
-    # TODO
+    
 
 
 if __name__ == "__main__":
