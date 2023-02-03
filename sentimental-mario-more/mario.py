2b89882c7cf51ab77da3ac1ce903a14f0d898447
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-03 05:07:51 +0000

    Fri, Feb 3, 2023, 12:07 AM -05:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,9 +1,14 @@
 # TODO
 from cs50 import get_int
 
-while num > 8 or num < 1
-    num = get_int("Height: ")
+num = get_height()
 
 for i in range(num)
-    for j = numin range(num)
+    for j = num - 1 and j in range(num)
         print
+
+def get_height()
+    while True:
+        num = get_int("Height: ")
+        if num > 0 and num < 9
+            return num
