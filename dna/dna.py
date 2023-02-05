3d3d42fb056de8317df04e79dd03e6e03d6be216
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-05 04:59:38 +0000

    Sat, Feb 4, 2023, 11:59 PM -05:00

--- dna/dna.py
+++ dna/dna.py
@@ -7,8 +7,9 @@ def main():
     # TODO: Check for command-line usage
     if len(sys.argv) != 3
         sys.exit("Usage: python dna.py data.csv sequence.txt")
-        
+
     # TODO: Read database file into a variable
+    
 
     # TODO: Read DNA sequence file into a variable
 
