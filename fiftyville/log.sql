a2886c045141157c21f756b970dbc5234ffc4a9d
t4goli <122705399+t4goli@users.noreply.github.com>
2023-02-13 07:27:22 +0000

    Mon, Feb 13, 2023, 2:27 AM -05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -332,8 +332,7 @@ WHERE year = 2021 AND day = 28 AND month = 7;
 -- bank transactions on day of theft
 SELECT amount, transaction_type, name
 FROM atm_transactions
-JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number
-JOIN people ON bank_accounts.person_id = people.id
+JOIN bank_accounts ON atm_transactions.account_number = bank_accounts.account_number JOIN people ON bank_accounts.person_id = people.id
 WHERE year = 2021 AND month = 7 AND day = 28 AND atm_location = "Leggett Street";
 /*
 +--------+------------------+---------+
