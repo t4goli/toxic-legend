1626b0ceb65e3387104c916de8cfa995f8e420ca
t4goli <122705399+t4goli@users.noreply.github.com>
2023-03-24 04:23:54 +0000

    Fri, Mar 24, 2023, 12:23 AM -04:00

--- finance/app.py
+++ finance/app.py
@@ -56,7 +56,7 @@ def buy():
             return apology("symbol does not exist", 403)
         if not symbol or shares < 0:
             return apology("gurlll", 403)
-        cash = db.execute("SELECT * FROM users WHERE username = ?", requests.Session())
+        cash = db.execute("SELECT cash FROM users WHERE username = ?", session["name"])
         return redirect("/")
     return apology("TODO")
 
