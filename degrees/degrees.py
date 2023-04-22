1d49171b4aaafabc61c19250502c432bb3e08b8b
t4goli <122705399+t4goli@users.noreply.github.com>
2023-04-22 16:23:41 +0000

    Sat, Apr 22, 2023, 12:23 PM -04:00

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -91,11 +91,49 @@ def shortest_path(source, target):
 
     If no possible path, returns None.
     """
-    start = Node(state=source, parent=None, action=None)
-    frontier = QueueFrontier()
-    frontier.add(start)
+     # Keep track of number of states explored
+        self.num_explored = 0
 
-    self.explored = set()
+        # Initialize frontier to just the starting position
+        start = Node(state=source, parent=None, action=None)
+        frontier = StackFrontier()
+        frontier.add(start)
+
+        # Initialize an empty explored set
+        self.explored = set()
+
+        # Keep looping until solution found
+        while True:
+
+            # If nothing left in frontier, then no path
+            if frontier.empty():
+                raise Exception("no solution")
+
+            # Choose a node from the frontier
+            node = frontier.remove()
+            self.num_explored += 1
+
+            # If node is the goal, then we have a solution
+            if node.state == self.goal:
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            # Mark node as explored
+            self.explored.add(node.state)
+
+            # Add neighbors to frontier
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action)
+                    frontier.add(child)
 
     # TODO
     raise NotImplementedError
