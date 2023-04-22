0a4c51ef2e3c33fb599397cfc27f90ef089062ac
t4goli <122705399+t4goli@users.noreply.github.com>
2023-04-22 17:21:15 +0000

    Sat, Apr 22, 2023, 1:21 PM -04:00

--- degrees/degrees.py
+++ degrees/degrees.py
@@ -133,7 +133,7 @@ def shortest_path(source, target):
         explored.add(node.state)
 
         # Add neighbors to frontier
-        for movie_id, person_id in neighbors(node.state):
+        for movie_id, person_id in neighbors_for_person(node.state):
             if not frontier.contains_state(person_id) and person_id not in explored:
                 child = Node(state=person_id, parent=node, action=movie_id)
                 frontier.add(child)
